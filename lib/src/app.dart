7eee26463b90f13df67d89067ee6bb937203ae34
julianmartire1 <julianmartire.jm@gmail.com>
2019-10-08 17:59:15 -0300

    app

---
+++ lib/src/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:probando/src/pages/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Center(child: HomePage()),
+    );
+  }
+}
---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final estiloTexto = TextStyle(fontSize: 25.0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Titulo'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Numero de clicks:', style: estiloTexto),
+            Text('0', style: estiloTexto),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('hola');
+        },
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+    );
+  }
+}
